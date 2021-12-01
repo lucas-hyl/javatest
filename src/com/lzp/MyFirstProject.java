509d1ecec2e5b0f02e310fc4d77cb647856382a0
lucas_git <lzp202437780@163.com>
2021-12-01 16:12:39 +0800

    first commit1

---
+++ src/com/lzp/MyFirstProject.java
@@ -0,0 +1,12 @@
+package com.lzp;
+
+public class MyFirstProject {
+    public static void main(String[] args) {
+        System.out.println("hello my new computer!!");
+        System.out.println("ni jue de wo shuo de you shen me wen ti ma ni shi zhen de liu bi");
+        System.out.println("ban ben kong zhi keyMap");
+        System.out.println("Key Map ke yi ji da de ti gao kai fa xiao lv");
+        System.out.println("idea he qi ta de kuai jie jian de shiyong fang ji ben shagn he wo men jar wen jian zhong you shen me bu yi gang ");
+        System.out.println("git demo");
+   }
+}
