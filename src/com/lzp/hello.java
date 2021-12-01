2a06eaf5363dfa13ab56706f22c6ea6a20b46dcb
lucas_git <lzp202437780@163.com>
2021-12-01 16:27:07 +0800

    hotfix commit1

--- src/com/lzp/hello.java
+++ src/com/lzp/hello.java
@@ -7,5 +7,7 @@ public class hello {
         System.out.println("hello git");
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello master");
+        System.out.println("hello hotfix");
     }
 }
